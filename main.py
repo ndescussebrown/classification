80bbca07e3b8fe4606db47537b9d6e1ff7c92f76
ndescussebrown <nathalie.descusse@gmail.com>
2022-04-12 11:30:08 +0200

    feat(main): added main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+import src.data.load_data as loader
+import src.models.cnn as classifier
+import os
+import tensorflow as tf
+from tensorflow import keras
+
+
+src='https://dsti-aws-class.s3.eu-west-3.amazonaws.com/kagglecatsanddogs_3367a.zip'
+dst='/content/cats_and_dogs/data/raw'
+
+loader.get_data(src,dst)
+loader.unzip_data(dst)
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join(dst,"PetImages",folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print("Deleted %d images" % num_skipped)
+
+image_size = (180, 180)
+batch_size = 32
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "/content/cats_and_dogs/data/raw/PetImages",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "/content/cats_and_dogs/data/raw/PetImages",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+model = classifier.make_model(input_shape=image_size + (3,), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True)
+
+epochs = 50
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+)
+
+
+
